26ebeeb35552e9e8241c1eb9273e9550ea69cde8
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-03-15 07:34:23 -0500

    this commit will define the sequelize associations in the index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+}); 
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', 
+})
 // Products belongToMany Tags (through ProductTag)
+// m to m relationship
+Product.belongsToMany(Tag, { through: ProductTag }); // Product belongsToMany Tags through the junction table 'ProductTag'.
 
 // Tags belongToMany Products (through ProductTag)
+// m to m relationship
+Tag.belongsToMany(Product, { through: ProductTag }); // Tag belongsToMany Products through the junction table 'ProductTag'. 
 
 module.exports = {
   Product,
