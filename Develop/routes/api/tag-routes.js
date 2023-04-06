fc910b9939e3dc2930cc5d963ea0f548448feedb
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-04-06 16:35:35 -0500

    this commit will complete routes for tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,55 +1,74 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // including its associated Product data
   try {
-    const productTag = await ProductTag.findAll();
-    res.status(200).json(productTag); 
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
+// find a single tag by its `id`
+router.get("/:id", async (req, res) => {
   // including its associated Product data
   try {
-    const productTag = await ProductTag.findByPk(req.params.id, {
-      include: [{model: Product}]
-    })
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "user not found" });
+      return;
+    }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
-  
-  
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-  
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', async(req, res) => {
-  // delete on tag by its `id` value
+// create a new tag
+router.post("/", async (req, res) => {
   try {
-    const productTag = await ProductTag.destroy({
+    const tagData = await ProductTag.create({ tagName: req.body.tagName });
+    res.status(200).json(tagData);
+    console.log("Successfully created Tag");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagData = await ProductTag.update(req.body, {
       where: {
         id: req.params.id,
-      }
+      },
     });
-    if(!productTag) {
-      res.status(404).json({message: 'no user found'})
-    };
+  } catch (err) {
+    res.send(400).json(err);
   }
-  catch {
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData = await ProductTag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "no user found" });
+    }
+  } catch {
     res.status(500).json(err);
   }
 });
